57d30046c727f72f51297ba4a138e65ff8993643
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2025-08-22 15:09:59 -0600

    I'm not sure Kernel 2 will coalesce global memory access

--- src/kernels/2_kernel_global_mem_coalesce.cuh
+++ src/kernels/2_kernel_global_mem_coalesce.cuh
@@ -11,4 +11,27 @@ __global__ void conv2d_global_mem_coalesce(int M, int N, const double *A, double
   const int cRow = blockIdx.x * BLOCKSIZE + threadIdx.y;
   const int cCol = blockIdx.y * BLOCKSIZE + threadIdx.x;
 
+  double filter[9] = {-1.0, -1.0, -1.0,
+          -1.0, 8.0, -1.0,
+          -1.0, -1.0, -1.0};
+  double avals[9] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+  A += i * N + j;
+
+  if ( (i < M) && (j < N) ) {
+
+    for (int fi = -1 ; fi < 2; fi++) {
+      for (int fj = -1; fj < 2; fj++) {
+        if ( (i + fi) >= 0 && (i + fi) < M && (j + fj) >= 0 && (j + fj) < N ) 
+          avals[(fi + 1) * 3 + (fj + 1)] = A[fi * N + fj];
+      }
+    }
+
+  double tmp = 0.0;
+  for (int floop = 0; floop < 9; floop++) 
+      tmp += avals[floop] * filter[floop];
+  B[i * N + j] = tmp;
+
+  }
+
 }
