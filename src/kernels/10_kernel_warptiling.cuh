c1ef690e6f4a59eccd3d2f54a0fa6b1e9ab8b3e7
Ganesh Vijayakumar <ganesh.vijayakumar@nrel.gov>
2025-08-22 14:36:41 -0600

    Remove implementation for a few kernels

--- src/kernels/10_kernel_warptiling.cuh
+++ src/kernels/10_kernel_warptiling.cuh
@@ -10,49 +10,6 @@
 #define CEIL_DIV(M, N) (((M) + (N)-1) / (N))
 const int WARPSIZE = 32; // warpSize is not constexpr
 
-namespace wt {
-template <const int BM, const int BN, const int BK, const int rowStrideA,
-          const int rowStrideB>
-__device__ void loadFromGmem(int M, int N, const double *A, double *B) {
-  for (uint offset = 0; offset + rowStrideA <= BM; offset += rowStrideA) {
-    const double4 tmp = reinterpret_cast<const double4 *>(
-        &A[(innerRowA + offset) * K + innerColA * 4])[0];
-    // double4 tmp;
-    // asm("ld.global.nc.v4.f32 {%0, %1, %2, %3}, [%4];"
-    //     : "=f"(tmp.x), "=f"(tmp.y), "=f"(tmp.z), "=f"(tmp.w)
-    //     : "l"(&A[(innerRowA + offset) * K + innerColA * 4]));
-    As[(innerColA * 4 + 0) * BM + innerRowA + offset] = tmp.x;
-    As[(innerColA * 4 + 1) * BM + innerRowA + offset] = tmp.y;
-    As[(innerColA * 4 + 2) * BM + innerRowA + offset] = tmp.z;
-    As[(innerColA * 4 + 3) * BM + innerRowA + offset] = tmp.w;
-  }
-
-  for (uint offset = 0; offset + rowStrideB <= BK; offset += rowStrideB) {
-    reinterpret_cast<double4 *>(
-        &Bs[(innerRowB + offset) * BN + innerColB * 4])[0] =
-        reinterpret_cast<const double4 *>(
-            &B[(innerRowB + offset) * N + innerColB * 4])[0];
-    // asm("ld.global.v4.f32 {%0, %1, %2, %3}, [%4];"
-    //     : "=f"(Bs[(innerRowB + offset) * BN + innerColB * 4 + 0]),
-    //       "=f"(Bs[(innerRowB + offset) * BN + innerColB * 4 + 1]),
-    //       "=f"(Bs[(innerRowB + offset) * BN + innerColB * 4 + 2]),
-    //       "=f"(Bs[(innerRowB + offset) * BN + innerColB * 4 + 3])
-    //     : "l"(&B[(innerRowB + offset) * N + innerColB * 4]));
-  }
-}
-
-template <const int BM, const int BN, const int BK, const int WM, const int WN,
-          const int WMITER, const int WNITER, const int WSUBM, const int WSUBN,
-          const int TM, const int TN>
-__device__ void
-processFromSmem(double *regM, double *regN, double *threadResults, const double *As,
-                const double *Bs, const uint warpRow, const uint warpCol,
-                const uint threadRowInWarp, const uint threadColInWarp) {
-
-}
-
-} // namespace wt
-
 /*
  * @tparam BM The threadblock size for M dimension SMEM caching.
  * @tparam BN The threadblock size for N dimension SMEM caching.
